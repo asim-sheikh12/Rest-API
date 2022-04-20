6e1d61d0966f63c1178fcbe8fbcb49f1dea35480
asim-thoughtwin <asim.thoughtwin@gmail.com>
2022-04-20 18:16:46 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -4,16 +4,22 @@ require("./db/connection");
 const Student = require("./models/students");
 const port = process.env.PORT || 8000;
 app.use(express.json());
-app.post("/students", (req, res) => {
-  const user = new Student(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((e) => {
-       res.status(400).send(e);
-    });
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createdStudent = await user.save();
+    res.status(201).send(createdStudent);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+app.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.status(200).send(studentsData);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 app.listen(port, () => {
   console.log(`Connected 👍 to ${port}`);
